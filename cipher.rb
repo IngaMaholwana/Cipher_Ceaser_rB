8e28dfa94932770e76b6a98297714a4ae318551a
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2025-01-13 20:00:32 +0200

    played around with new learnt thing rubocop and made a yaml playing around with the departments

--- cipher.rb
+++ cipher.rb
@@ -1,17 +1,17 @@
+# frozen_string_literal: true
+
 def ceasar_cipher(phrase, shift)
-  phrase_array = phrase.split("")
+  phrase_array = phrase.split('')
   phrase_array.map! do |char|
     if char.ord.between?(65, 90) || char.ord.between?(97, 122)
       char = char.ord + shift
-      if char > 122 || (char > 90 && char < 97)
-        char = char - 26
-      end
-      char = char.chr
+      char -= 26 if char > 122 || (char > 90 && char < 97)
+      char.chr
     else
       char
     end
   end
-  phrase_array.join("")
+  phrase_array.join('')
 end
 
-puts ceasar_cipher("molweni apho kuni", 5)
+puts ceasar_cipher('molweni apho kuni', 5)
